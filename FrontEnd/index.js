652ccb7982dbe59f8a2c776ae4ffe890dab18ce1
Geoffrey Coste <geoffreycoste13@gmail.com>
2022-11-10 10:59:39 +0100

    Filter feature first version

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,27 +1,113 @@
+let portfolio = document.querySelector('#portfolio');
 let gallery = document.querySelector('.gallery');
+let works;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const getWorksData = () => {
-        fetch('http://localhost:5678/api/works')
-        .then((res) => {
-            if(res.ok) {
-                return res.json();
-            }
-        })
-        .then((works) => {
-            works.forEach(work => createFigureElement(work));
-        })
-        .catch(e => console.log(e));
-    };
+/* Getting API works data and display */
+const fetchWorksData = async () => {
+    try {
+        const response = await fetch('http://localhost:5678/api/works');
+        works = await response.json();
+        if(!Array.isArray(works)) {
+            works = [works];
+        }
+        displayWorks(works);
+        createFilterMenu(works);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-    getWorksData();
-});
+/* Displaying image gallery as from API works data */
+const displayWorks = (works) => {
+    const worksNodes = works.map((work) => {
+        return createWorkElement(work);
+    });
+    gallery.innerHTML = "";
+    gallery.append(...worksNodes);
+};
 
-const createFigureElement = (obj) => {
+const createWorkElement = (work) => {
     const figure = document.createElement('figure');
     figure.innerHTML = `
-        <img src=${obj.imageUrl} alt=${obj.title}>
-        <figcaption>${obj.title}</figcaption>
-    `
-    gallery.append(figure);
+        <img src=${work.imageUrl} alt=${work.title}>
+        <figcaption>${work.title}</figcaption>
+    `;
+    return figure;
 };
+
+
+/* Creation of filter menu and buttons inside */
+const createFilterMenu = (works) => {
+    const filterMenu = document.createElement('div');
+    filterMenu.classList.add('filters');
+
+    const btnFilterAll = document.createElement('button');
+    btnFilterAll.classList.add('btn-filter', 'active');
+    btnFilterAll.dataset.name = 'tous';
+    btnFilterAll.innerText = 'Tous';
+
+    const btnFilterCategoriesNodes = works
+                                        .reduce((names, obj) => {
+                                            if (!names.includes(obj.category.name)) {
+                                                names.push(obj.category.name)
+                                            }
+                                            return names;
+                                        }, [])
+                                        .map((name) => {
+                                            return createCategoryFilterBtn((name));
+                                        });
+    
+    portfolio.insertBefore(filterMenu, gallery);
+    filterMenu.innerHTML = "";
+    filterMenu.append(btnFilterAll, ...btnFilterCategoriesNodes);
+    
+    const btnFilters = [btnFilterAll, ...btnFilterCategoriesNodes];
+    handleFilters(btnFilters, works);
+    console.log(btnFilters);
+}
+
+const createCategoryFilterBtn = (name) => {
+    const categoryFilterBtn = document.createElement('button');
+    categoryFilterBtn.classList.add('btn-filter');
+    categoryFilterBtn.dataset.name = name.toLowerCase();
+    categoryFilterBtn.innerText = name;
+    return categoryFilterBtn;
+};
+
+/* Filtering works displayed in gallery */
+const handleFilters = (btnFiltersArr, works) => {
+    console.log(works);
+    btnFiltersArr.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            toggleActive(event);
+            filterWorks(event, works);
+        });
+    });
+
+    const filterWorks = (event, works) => {
+        let btnFilter = event.target;
+        console.log(works);
+        if (btnFilter.dataset.name !== 'tous') {
+            const filteredWorks = works.filter(work => work.category.name.toLowerCase() === btnFilter.dataset.name);
+            console.log(filteredWorks);
+            return displayWorks(filteredWorks);
+        } else {
+            return displayWorks(works);
+        };
+    }
+
+    const toggleActive = (event) => {
+        let target = event.target;
+        btnFiltersArr.forEach(btn => {
+            if (btn === target && !btn.classList.contains('active')) {
+                btn.classList.add('active');
+            } else if (btn === target && btn.classList.contains('active')) {
+                return
+            } else {
+                btn.classList.remove('active');
+            }
+        });
+    };
+};
+
+fetchWorksData();
